3d15446e76b4049b3a6e83488795371b6fd25a2f
TheCubbe <199993614+thecubbe@users.noreply.github.com>
2025-09-14 02:08:43 +0200

    Update env.py

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -110,11 +110,11 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Fullvio WebUI")
+if WEBUI_NAME != "Fullvio WebUI":
+    WEBUI_NAME += " WebUI"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://github.com/84591486748a47bde1f01f355963135c9561269/images/blob/main/cf-access/favicon.png"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
